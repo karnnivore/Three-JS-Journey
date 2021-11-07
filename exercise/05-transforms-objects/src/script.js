3c06ddd1c8a3c9b9482618ad62dad7bdcd95c25c
Nick Chinsen <nicholaschinsen@gmail.com>
2021-11-06 20:56:33 -0400

    05 - Transforming objects in three.js

---
+++ exercise/05-transforms-objects/src/script.js
@@ -0,0 +1,73 @@
+import './style.css'
+import * as THREE from 'three'
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Objects
+ */
+// Scene group
+const group = new THREE.Group()
+scene.add(group)
+
+// Create three cubes to add to scene group
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0x00ff00})
+)
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0x0000ff })
+)
+
+// Add cubes and set positions
+group.add(cube1)
+cube2.position.set(2, 0, 0)
+group.add(cube2)
+cube3.position.set(-2, 0, 0)
+group.add(cube3)
+
+// Move entire group
+group.position.y = 1
+group.scale.y = 2
+group.rotation.y = 1
+
+// Axes helper
+const axesHelper = new THREE.AxesHelper(5)
+scene.add(axesHelper)
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+/**
+ * Camera
+ */
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+
+// Look at a Vector3 target
+// camera.lookAt(mesh.position)
+
+scene.add(camera)
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
